3b68170f94b4a74ecd919838d0ddd5240b85d8d8
amananurag <aman.anurag@sensationsolutions.in>
2025-09-05 11:22:07 +0530

    done arrow wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ const App = () => {
   const [data, setData] = useState(1000);
   const [products, setProducts] = useState([]);
 
-  const handleClick = () => {
-    setCount(count + 1000);
+  const handleClick = (action) => {
+    if (action === "increase") {
+      return setCount(count + 1000);
+    }
+
+    setCount(count - 1000);
   };
 
   const handleData = () => {
@@ -19,9 +23,9 @@ const App = () => {
     try {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
-      const newData=data.map((item)=>{
-        return {...item,num:crypto.randomUUID()}
-      })
+      const newData = data.map((item) => {
+        return { ...item, num: crypto.randomUUID() };
+      });
       console.log(newData);
       setProducts(newData);
     } catch (error) {
@@ -29,24 +33,25 @@ const App = () => {
     }
   };
 
-
-
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <div className="container">
-      {products.map((item,index) => {
-        return (
-          <Product key={item.num} item={item} />
-        );
+      {products.map((item, index) => {
+        return <Product key={item.num} item={item} />;
       })}
 
       <h1>count value is {count}</h1>
       <h1>data value is {data}</h1>
 
-      <button onClick={handleClick}>click me count</button>
+      <button
+        onClick={() => handleClick("increase")}      
+      >
+        increase count
+      </button>
+      <button onClick={handleClick}>decrease count</button>
       <button onClick={handleData}>click me data</button>
     </div>
   );
