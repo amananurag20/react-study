a34cd256f5a758427e328d4356648637bb03bc19
amananurag <aman.anurag@sensationsolutions.in>
2025-09-08 11:33:27 +0530

    multiple state

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,68 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [email, setEmail]=useState("")
-  const [password, setPassword]= useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [phone, setPhone] = useState("");
+  // const [address, setAddress]= useState("")//
+
+  const [formData, setFromData] = useState({
+    email: "",
+    password: "",
+    address: "",
+    phone: "",
+  });
 
   const handleClick = () => {
-   setEmail("aman@gmail.com")
+    alert("submitted");
   };
-
-  console.log(email)
 
   const handleChange = (e) => {
-   setPassword(e.target.value)
+    console.log(e);
+    const { name, value } = e.target;
+
+    setFromData({ ...formData, [name]: value });
   };
 
+const handleSubmit=(e)=>{
+  e.preventDefault()
+}
 
   return (
     <div className="container">
-      <label htmlFor="email">Enter your email</label>
-      <input placeholder="your name" type="text" id="email"
-      value={email}
-      onChange={(e)=>setEmail(e.target.value)}
-       />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Enter your email</label>
+        <input
+          placeholder="your name"
+          type="text"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+          name="email"
+        />
 
-      <label htmlFor="password">Password</label>
-      <input
-        placeholder="your name"
-        type="password"
-        id="password"
-        onChange={handleChange}
-        value={password}
-      />
+        <label htmlFor="password">Password</label>
+        <input
+          placeholder="your name"
+          type="password"
+          id="password"
+          onChange={handleChange}
+          value={formData.password}
+          name="password"
+        />
 
-      <button onClick={handleClick}>Submit form</button>
+        <label htmlFor="phone">phone no</label>
+        <input
+          placeholder="your name"
+          type="number"
+          id="phone"
+          onChange={handleChange}
+          value={formData.phone}
+          name="phone"
+        />
+
+        <button  onClick={handleClick}>Submit form</button>
+      </form>
     </div>
   );
 };
