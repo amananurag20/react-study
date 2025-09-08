735bec0d7e617ff19c8d5ce3b211f1d353492432
amananurag <aman.anurag@sensationsolutions.in>
2025-09-08 10:27:39 +0530

    input fled value

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import Product from "./components/Product/Product";
 
 const App = () => {
-  const [count, setCount] = useState(1000);
-  const [data, setData] = useState(1000);
-  const [products, setProducts] = useState([]);
+  const [email, setEmail]=useState("")
+  const [password, setPassword]= useState("");
 
-  const handleClick = (action) => {
-    if (action === "increase") {
-      return setCount(count + 1000);
-    }
-
-    setCount(count - 1000);
+  const handleClick = () => {
+   setEmail("aman@gmail.com")
   };
 
-  const handleData = () => {
-    setData(data + 1000);
+  console.log(email)
+
+  const handleChange = (e) => {
+   setPassword(e.target.value)
   };
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      const newData = data.map((item) => {
-        return { ...item, num: crypto.randomUUID() };
-      });
-      console.log(newData);
-      setProducts(newData);
-    } catch (error) {
-      console.log("Error fetching data:", error.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <div className="container">
-      {products.map((item, index) => {
-        return <Product key={item.num} item={item} />;
-      })}
+      <label htmlFor="email">Enter your email</label>
+      <input placeholder="your name" type="text" id="email"
+      value={email}
+      onChange={(e)=>setEmail(e.target.value)}
+       />
 
-      <h1>count value is {count}</h1>
-      <h1>data value is {data}</h1>
+      <label htmlFor="password">Password</label>
+      <input
+        placeholder="your name"
+        type="password"
+        id="password"
+        onChange={handleChange}
+        value={password}
+      />
 
-      <button
-        onClick={() => handleClick("increase")}      
-      >
-        increase count
-      </button>
-      <button onClick={handleClick}>decrease count</button>
-      <button onClick={handleData}>click me data</button>
+      <button onClick={handleClick}>Submit form</button>
     </div>
   );
 };
